5087852e3104160ef056490dc360d30448ca5eec
YenYoong <yenyoong@alsonmedia.com>
2024-01-21 22:26:42 +0800

    add user interface test

---
+++ apps/user/interface_test.go
@@ -0,0 +1,19 @@
+package user_test
+
+import (
+	"testing"
+
+	"goProjects/yyblog/apps/user"
+)
+
+// $2a$10$1MvkjvWOS0/Rf.cEKKxeie/Y7ADz9XZTq09Wd/bKwX/vUv0kdYJ4.
+// $2a$10$IyB.w1NVOrBmZ9WOsT6gEuruaynjse2CNmce9399yUErnufV10DX2
+// https://gitee.com/infraboard/go-course/blob/master/day09/go-hash.md#bcrypt
+func TestHashPassword(t *testing.T) {
+	req := user.NewCreateUserRequest()
+	req.Password = "123456"
+	u := user.NewUser(req)
+	t.Log(u.Password)
+
+	t.Log(u.CheckPassword("123456"))
+}
