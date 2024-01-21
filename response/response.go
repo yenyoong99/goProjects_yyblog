4f0c3000bbd09a8c2ad3f24db2486909f36590f1
YenYoong <yenyoong@alsonmedia.com>
2024-01-21 23:14:53 +0800

    add response func

--- response/response.go
+++ response/response.go
@@ -1,1 +1,33 @@
 package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"goProjects/yyblog/exception"
+)
+
+// Success API, return data
+func Success(c *gin.Context, data any) {
+	c.JSON(http.StatusOK, data)
+}
+
+// Failed API return err, API Exception
+func Failed(c *gin.Context, err error) {
+	// Construct exception data
+	var resp *exception.APIException
+	if e, ok := err.(*exception.APIException); ok {
+		resp = e
+	} else {
+		resp = exception.NewAPIException(
+			500,
+			http.StatusText(http.StatusInternalServerError),
+		).WithMessage(err.Error()).WithHttpCode(500)
+	}
+
+	// return exception
+	c.JSON(resp.HttpCode, resp)
+
+	// abort
+	c.Abort()
+}
