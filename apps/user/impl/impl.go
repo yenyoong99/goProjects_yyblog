1f0e2db1f300e7c9658501508c5d21d6289fe445
YenYoong <yenyoong@alsonmedia.com>
2024-01-21 22:30:00 +0800

    add user app implement

--- apps/user/impl/impl.go
+++ apps/user/impl/impl.go
@@ -1,8 +1,20 @@
 package impl
 
-// UserServiceImpl
-// 怎么实现user.Service接口?
-// 定义UserServiceImpl来实现接口
-type UserServiceImpl struct {
+import (
+	"goProjects/yyblog/conf"
+	"gorm.io/gorm"
+)
+
+func NewUserServiceImpl() *UserServiceImpl {
+	return &UserServiceImpl{
+		//get global configurations db object
+		db: conf.C().DB(),
+	}
+
 }
 
+// UserServiceImpl
+// define UserServiceImpl to implement the interface
+type UserServiceImpl struct {
+	db *gorm.DB
+}
