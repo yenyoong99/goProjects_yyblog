19dc4e4e7b9e60dff1e07a790a14572586858a16
YenYoong <yy@yybloger.com>
2024-01-27 22:09:33 +0800

    add apps blog business exception

---
+++ exception/business.go
@@ -0,0 +1,8 @@
+package exception
+
+import "net/http"
+
+var (
+	ErrBadRequest   = NewAPIException(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).WithHttpCode(http.StatusBadRequest)
+	ErrUnauthorized = NewAPIException(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized)).WithHttpCode(http.StatusUnauthorized)
+)
