e814fdc8d34b81f07dbf28c483bfbd17814fc005
YenYoong <yy@yybloger.com>
2024-02-17 12:10:32 +0800

    split permission middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,8 +3,6 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/yenyoong99/goProjects_yyblog/apps/token"
-	"github.com/yenyoong99/goProjects_yyblog/apps/user"
-	"github.com/yenyoong99/goProjects_yyblog/exception"
 	"github.com/yenyoong99/goProjects_yyblog/ioc"
 	"github.com/yenyoong99/goProjects_yyblog/response"
 )
@@ -23,29 +21,4 @@ func Auth(c *gin.Context) {
 	c.Set(token.TOKEN_MIDDLEWARE_KEY, tkObj)
 
 	// c.Get(token.TOKEN_MIDDLEWARE_KEY).(*token.Token).UserName
-}
-
-// Required Define user role permission
-func Required(roles ...user.Role) gin.HandlerFunc {
-	return func(context *gin.Context) {
-		// verify user role
-		// token get user role, define the interface role
-		if v, ok := context.Get(token.TOKEN_MIDDLEWARE_KEY); ok {
-			hasPerm := false
-			// traverse to determine whether the user is in the role list
-			for _, r := range roles {
-				//fmt.Println(roles, v.(*token.Token).Role)
-				if r == v.(*token.Token).Role {
-					hasPerm = true
-				}
-			}
-
-			if !hasPerm {
-				response.Failed(context, exception.ErrPermissionDeny.WithMessagef("user role not allow access"))
-				return
-			}
-		} else {
-			response.Failed(context, exception.ErrUnauthorized)
-		}
-	}
 }
