8ed7db172ea748f2ea8d21eda75aa82b8f3c3c90
YenYoong <yy@yybloger.com>
2024-03-16 18:12:42 +0800

    update request exception error

--- backend/exception/business.go
+++ backend/exception/business.go
@@ -3,7 +3,7 @@ package exception
 import "net/http"
 
 var (
-	ErrBadRequest     = NewAPIException(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).WithHttpCode(http.StatusBadRequest)
-	ErrUnauthorized   = NewAPIException(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized)).WithHttpCode(http.StatusUnauthorized)
-	ErrPermissionDeny = NewAPIException(http.StatusForbidden, http.StatusText(http.StatusForbidden)).WithHttpCode(http.StatusForbidden)
+	ErrBadRequest     = NewAPIException(http.StatusBadRequest, http.StatusText(http.StatusBadRequest)).WithHttpCode(http.StatusBadRequest).WithMessage("Bad Request.")
+	ErrUnauthorized   = NewAPIException(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized)).WithHttpCode(http.StatusUnauthorized).WithMessage("Please Login.")
+	ErrPermissionDeny = NewAPIException(http.StatusForbidden, http.StatusText(http.StatusForbidden)).WithHttpCode(http.StatusForbidden).WithMessage("Access Denied.")
 )
